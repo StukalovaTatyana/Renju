d21b7e1dd798684f4ae6656628ea8606ed867b3a
Aleksey Shiyanov <Shiyanov1998@mail.ru>
2018-05-31 15:05:19 +0400

    ADD AI BLEAT'

--- src/sample/AI.java
+++ src/sample/AI.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 
 public class AI {
 
-    private int my;
+    public int my;
     private int enemy;
-    private Mover mover;
+    private Mover mover = new Mover();
     private ArrayList<SuperPosition> myPos;
     private ArrayList<SuperPosition> enemyPos;
+    private Controller controller;
 
     public AI(int my, int enemy) {
         this.my = my;
@@ -146,29 +147,181 @@ public class AI {
         return -1;
     }
 
-    public void moveAI() {
-        analizeBoard();
-        ArrayList<SuperPosition> listPos = getPosWithNumber(my, 4);
-        if (listPos.size() != 0) {
-            for (SuperPosition pos : listPos) {
-                int num = getNumber(pos, 4);
-                if (num == 0) {
-                    int pointx = pos.coord.x;
-                    int pointy = pos.coord.y;
-                    while (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 &&
-                            Mover.board[pointx + 1][pointy + 1] != 0 && Mover.board[pointx + 1][pointy + 1] != enemy) {
-                        pointx++;
-                        pointy++;
+    public Point findAndMove(int player, ArrayList<SuperPosition> listPos, int number) {
+        for (SuperPosition pos : listPos) {
+            int num = getNumber(pos, number);
+            if (num == 0) {
+                int pointx = pos.coord.x;
+                int pointy = pos.coord.y;
+                while (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 &&
+                        Mover.board[pointx + 1][pointy + 1] != 0 && Mover.board[pointx + 1][pointy + 1] != player) {
+                    pointx++;
+                    pointy++;
+                }
+                if (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 && Mover.board[pointx + 1][pointy + 1] != player) {
+                    mover.point(pointx + 1, pointy + 1);
+                    //controller.addImage(my, pointx + 1, pointy + 1);
+                    mover.winGame(pointx + 1, pointy + 1);
+                    return new Point(pointx+1, pointy+1);
+                } else {
+                    pointx = pos.coord.x;
+                    pointy = pos.coord.y;
+                    while (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 &&
+                            Mover.board[pointx - 1][pointy - 1] != 0 && Mover.board[pointx - 1][pointy - 1] != player) {
+                        pointx--;
+                        pointy--;
                     }
-                    if (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 && Mover.board[pointx + 1][pointy + 1] != enemy) {
-                        mover.point(pos.coord.x, pos.coord.y);
+                    if (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 && Mover.board[pointx - 1][pointy - 1] != player) {
+                        mover.point(pointx - 1, pointy - 1);
+                        //controller.addImage(my, pointx - 1, pointy - 1);
+                        mover.winGame(pointx - 1, pointy - 1);
+                        return new Point(pointx-1, pointy-1);
+                    }
+                }
+            } else if (num == 1) {
+                int pointx = pos.coord.x;
+                int pointy = pos.coord.y;
+                while (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 &&
+                        Mover.board[pointx - 1][pointy + 1] != 0 && Mover.board[pointx - 1][pointy + 1] != player) {
+                    pointx--;
+                    pointy++;
+                }
+                if (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 && Mover.board[pointx - 1][pointy + 1] != player) {
+                    mover.point(pointx - 1, pointy + 1);
+                    //controller.addImage(my, pointx - 1, pointy + 1);
+                    mover.winGame(pointx - 1, pointy + 1);
+                    return new Point(pointx-1, pointy+1);
+                } else {
+                    pointx = pos.coord.x;
+                    pointy = pos.coord.y;
+                    while (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 &&
+                            Mover.board[pointx + 1][pointy - 1] != 0 && Mover.board[pointx + 1][pointy - 1] != player) {
+                        pointx++;
+                        pointy--;
+                    }
+                    if (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 && Mover.board[pointx + 1][pointy - 1] != player) {
+                        mover.point(pointx + 1, pointy - 1);
+                        //controller.addImage(my, pointx + 1, pointy - 1);
+                        mover.winGame(pointx + 1, pointy - 1);
+                        return new Point(pointx+1, pointy-1);
+                    }
+                }
+            } else if (num == 2) {
+                int pointx = pos.coord.x;
+                int pointy = pos.coord.y;
+                while (pointx >= 0 && pointx <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 &&
+                        Mover.board[pointx][pointy + 1] != 0 && Mover.board[pointx][pointy + 1] != player) {
+                    pointy++;
+                }
+                if (pointx >= 0 && pointx <= 15 && pointy + 1 >= 0 && pointy + 1 <= 15 && Mover.board[pointx][pointy + 1] != player) {
+                    mover.point(pointx, pointy + 1);
+                    //controller.addImage(my, pointx, pointy + 1);
+                    mover.winGame(pointx, pointy + 1);
+                    return new Point(pointx, pointy+1);
+                } else {
+                    pointx = pos.coord.x;
+                    pointy = pos.coord.y;
+                    while (pointx >= 0 && pointx <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 &&
+                            Mover.board[pointx][pointy - 1] != 0 && Mover.board[pointx][pointy - 1] != player) {
+                        pointy--;
+                    }
+                    if (pointx >= 0 && pointx <= 15 && pointy - 1 >= 0 && pointy - 1 <= 15 && Mover.board[pointx][pointy - 1] != player) {
+                        mover.point(pointx, pointy - 1);
+                        //controller.addImage(my, pointx, pointy - 1);
+                        mover.winGame(pointx, pointy - 1);
+                        return new Point(pointx, pointy-1);
+                    }
+                }
+            } else if (num == 3) {
+                int pointx = pos.coord.x;
+                int pointy = pos.coord.y;
+                while (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy >= 0 && pointy <= 15 &&
+                        Mover.board[pointx + 1][pointy] != 0 && Mover.board[pointx + 1][pointy] != player) {
+                    pointx++;
+                }
+                if (pointx + 1 >= 0 && pointx + 1 <= 15 && pointy >= 0 && pointy <= 15 && Mover.board[pointx + 1][pointy] != player) {
+                    mover.point(pointx + 1, pointy);
+                    //controller.addImage(my, pointx + 1, pointy);
+                    mover.winGame(pointx + 1, pointy);
+                    return new Point(pointx+1, pointy);
+                } else {
+                    pointx = pos.coord.x;
+                    pointy = pos.coord.y;
+                    while (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy >= 0 && pointy <= 15 &&
+                            Mover.board[pointx - 1][pointy] != 0 && Mover.board[pointx - 1][pointy] != player) {
+                        pointx--;
+                    }
+                    if (pointx - 1 >= 0 && pointx - 1 <= 15 && pointy >= 0 && pointy <= 15 && Mover.board[pointx - 1][pointy] != player) {
+                        mover.point(pointx - 1, pointy);
+                        //controller.addImage(my, pointx - 1, pointy);
+                        mover.winGame(pointx - 1, pointy);
+                        return new Point(pointx-1, pointy);
                     }
                 }
             }
         }
-        if () {
+        return null;
+    }
 
+    // враг для нас, мы для врага
+    // getPosWithNumber(my, 4); для поиска в нашем массиве
+    public Point moveAI() {
+        analizeBoard();
+        // ищем наши четверки
+        ArrayList<SuperPosition> listPos = getPosWithNumber(my, 4);
+        if (listPos.size() != 0) { // если они есть, то ходим и по возможности побеждаем
+            findAndMove(enemy, listPos, 4);
         }
+        listPos = getPosWithNumber(enemy, 4);
+        if (listPos.size() != 0) {
+            Point a = findAndMove(my, listPos, 4);
+            if (a != null) {
+                mover.changePlayer();
+                return a;
+            }
+        }
+        listPos = getPosWithNumber(enemy, 3);
+        if (listPos.size() != 0) {
+            Point a = findAndMove(my, listPos, 3);
+            if (a != null) {
+                mover.changePlayer();
+                return a;
+            }
+        }
+        listPos = getPosWithNumber(my, 3);
+        if (listPos.size() != 0) {
+            Point a = findAndMove(enemy, listPos, 3);
+            if (a != null) {
+                mover.changePlayer();
+                return a;
+            }
+        }
+        listPos = getPosWithNumber(my, 2);
+        if (listPos.size() != 0) {
+            Point a = findAndMove(enemy, listPos, 2);
+            if (a != null) {
+                mover.changePlayer();
+                return a;
+            }
+        }
+        listPos = getPosWithNumber(my, 1);
+        if (listPos.size() != 0) {
+            Point a = findAndMove(enemy, listPos, 1);
+            if (a != null) {
+                mover.changePlayer();
+                return a;
+            }
+        }
+        int a = 7 + (int) (Math.random() * 10);
+        int b = 7 + (int) (Math.random() * 10);
+        while (mover.canPoint(a, b) == false) {
+            a = 7 + (int) (Math.random() * 10);
+            b = 7 + (int) (Math.random() * 10);
+        }
+        mover.point(a, b);
+        mover.changePlayer();
+        return new Point(a, b);
+
     }
 
 }
